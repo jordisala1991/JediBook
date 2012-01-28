b4c494f2efd44951f159c812dfda421cca968185
elenaVazquez <evazquezdaza@gmail.com>
2012-01-28 17:42:37 +0100

    classes usuari i foto

---
+++ Classes/Foto.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Foto
+ *
+ * @author elena_gratallops
+ */
+class Foto {
+    //put your code here
+}
+
+?>
---
+++ Classes/Usuari.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Usuari
+ *
+ * @author elena_gratallops
+ */
+class Usuari {
+    //put your code here
+}
+
+?>
