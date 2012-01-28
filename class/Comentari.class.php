d79e2afad69edce468e39f20d4f6c780dc654b91
Jordi <jordisala1991@gmail.com>
2012-01-28 18:08:04 +0100

    Comentari.class.php afegida

---
+++ class/Comentari.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Comentari
+ *
+ * @author Jordi
+ */
+class Comentari {
+    //put your code here
+}
+
+?>
