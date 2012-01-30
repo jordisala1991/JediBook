f1610be0a99769f977f9235f20844422da1c0efd
Jordi <jordisala1991@gmail.com>
2012-01-30 16:27:56 +0100

    Classe per conectarse a la bd implementada!

--- class/JediBookBD.class.php
+++ class/JediBookBD.class.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of JediBookBD
  *
@@ -17,15 +12,33 @@ class JediBookBD {
     protected $db;
     protected $host;
     
-    
     function __construct($host, $user, $pass, $db) {
         $this->db = $db;
         $this->host = $host;
         $this->pass = $pass;
         $this->user = $user;
-        mysql_connect($host, $user, $pass); 
+        $this->idConnection = mysql_connect($host, $user, $pass); 
+        mysql_select_db($db, $this->idConnection);
     }
     
+    function selectSQL($query) {
+        $consulta = mysql_query($query, $this->idConnection);
+        $res = array();
+        $aux = array();
+        while (($aux = mysql_fetch_assoc($consulta)) !== false) {
+            $res[] = $aux;
+        }
+        return $res;
+    }
+    
+    function allSQL($query){
+        mysql_query($query, $this->idConnection);
+        return mysql_affected_rows($this->idConnection);
+    }
+    
+    function close() {
+        mysql_close($this->idConnection);
+    }
 }
 
 ?>
