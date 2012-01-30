01adf520026d931d83769e8dedf495c6a96c75a2
elenaVazquez <evazquezdaza@gmail.com>
2012-01-30 11:55:44 +0100

    JediBookBD

---
+++ class/JediBookBD.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of JediBookBD
+ *
+ * @author elena_gratallops
+ */
+class JediBookBD {
+    //put your code here
+}
+
+?>
