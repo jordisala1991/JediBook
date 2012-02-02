e643107732c1b57d585de693409be15df60f6d9c
elenaVazquez <evazquezdaza@gmail.com>
2012-02-02 14:14:16 +0100

    gestioFotos

--- class/GestioFotos.php
+++ class/GestioFotos.php
@@ -4,14 +4,53 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+define("RUTA_FOTOS", 'Imatges/');
+define("RUTA_FOTOS_PERFIL", 'ImatgesPerfil/');
+//define("TAM_MAX_PERFIL", 150);
+define("TAM_MAX", 8000000);
 /**
+ * 
  * Description of GestioFotos
  *
  * @author elena_gratallops
  */
 class GestioFotos {
-    //put your code here
+    
+    protected $rutaTemporal;
+    protected $nom;
+    protected $tipus;
+    protected $error;
+    protected $tamany;
+    protected $esPerfil;
+
+    function __construct($file, $esPerfil) {
+        $this->nom = $file[0];
+        $this->tipus = $file[1];
+        $this->rutaTemporal = $file[2];
+        $this->error = $file[3];
+        $this->tamany = $file[4];
+        $this->esPerfil = $esPerfil;
+    }
+    
+    function save(){
+        if($this->error == 0) {
+            if($this->tamany <= TAM_MAX){
+                if($this->tipus == 'image/jpeg' or $this->tipus == 'image/png' or $this->tipus == 'image/gif'){
+                    if($this->esPerfil){
+                        if (move_uploaded_file($this->rutaTemporal, RUTA_FOTOS_PERFIL.$this->nom)) {
+                            return RUTA_FOTOS_PERFIL.$this->nom;
+                        }
+                    }
+                    else if (move_uploaded_file ($this->rutaTemporal, RUTA_FOTOS.$this->nom)){
+                        return RUTA_FOTOS.$this->nom; 
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+    
+    
 }
 
 ?>
