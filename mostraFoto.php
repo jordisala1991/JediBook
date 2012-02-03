c7785a24c00f6a36be1f34bc7c24197cf26b7404
elenaVazquez <evazquezdaza@gmail.com>
2012-02-03 10:56:28 +0100

    mostraFoto i csss

--- mostraFoto.php
+++ mostraFoto.php
@@ -1,17 +1,27 @@
 <?php
-if(!isset($_GET['id'])) {
-    header("Location: perfil.php");
-}
-else {
     include_once 'class/Foto.class.php';
     include_once 'class/Comentari.class.php';
     include_once 'class/JediBookBD.class.php';
-    $foto = new Foto((int)$_GET['id']);
-    $comentaris = array();
-    $bd = new JediBookBD();
-    $comentaris[] = $bd->getComentaris($_GET['id']);
-    $bd->close();
-}
+    if(!isset($_GET['id'])) {
+        header("Location: perfil.php");
+    }
+    else {
+
+        $foto = new Foto((int)$_GET['id']);
+        $comentaris = array();
+        $bd = new JediBookBD();
+        $comentaris[] = $bd->getComentaris((int)$_GET['id']);
+        //$bd->close();
+    }
+
+    if(isset ($_POST['votsOK'])){
+        $foto->incrementaVotsOK();
+        $foto->update();
+    }
+    if(isset ($_POST['votsKO'])){
+        $foto->incrementaVotsKO();
+        $foto->update();
+    }
 
 ?>
 <!DOCTYPE html>
@@ -30,25 +40,40 @@ else {
             <div id="main">
                 <div id="columna">
                     <div id="titol"><h3>Foto</h3></div>
-                    <div class="foto"><img src="<?php echo $f->getFoto();?>" alt="mail image" width="500" height="500" border="0" boder="0" /></div>
-                    <div id="descripcio"><label for="descripcio"><?php echo $f->getDescripcio();?></label></div>
-<!--                    <div id="comentari"><label for="comentari">-->
+                    <div class="foto"><img src="<?php echo $foto->getFoto();?>" alt="mail image" width="500" height="500" border="0" boder="0" /></div>
+                    <div id="descripcio"><label for="descripcio"><?php echo $foto->getDescripcio();?></label></div>
+                    <?php
+                        echo '<div id="votsOK">';
+                        echo '<form action="mostraFoto.php?id='.$_GET["id"].'" method="post">';
+                        echo '<input type="submit" class="minimal" name="votsOK" value="VotsOK">';
+                        echo '</form>';
+                        echo '</div>';
+                    ?>
                     <?php 
-                        foreach ($comentaris as $c){
-                               $t = $c->getText();
-                               echo $t;
+                        echo '<div id="votsKO">';
+                        echo '<form action="mostraFoto.php?id='.$_GET["id"].'" method="post">';
+                        echo '<input type="submit" class="minimal" name="votsKO" value="VotsKO">';
+                        echo '</form>';
+                        echo '</div>';
+                    
+                    ?>
+                    <?php 
+                        for ($i = 0; $i < sizeof($comentaris); ++$i){
+                               echo '<div class="comentari">';
+                               $u = $comentaris[0][$i]->getUsuari();
+                               echo '<p>';
+                               echo $u->getUserName();
+                               echo $comentaris[0][$i]->getData();
                                echo " ";
-                               $d = $c->getData();
-                               echo $d;
+                               echo '</p>';
+                               echo '<p>';
+                               echo $comentaris[0][$i]->getText();
                                echo " ";
-                               $u = $c->getUsuari();
-                               $us = $u->getUserName();
-                               echo $us;
-                               echo " ";
+                               echo '</p>';
+                               echo '</div>';
                                echo '<br><br>';
                         }
-                    ?>
-<!--</label></div>-->
+                    ?>  
                 </div>
                 <br style="clear:both;">
             </div>
